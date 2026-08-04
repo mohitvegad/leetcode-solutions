16a069c5f0f8373ce7ad6f8ed537b24d2de79499
mohitvegad <kfdvhtq424@privaterelay.appleid.com>
2026-08-04 23:24:53 +0100

    Time: 16 ms (29.13%), Space: 21.2 MB (76.7%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func findDuplicate(_ nums: [Int]) -> Int {
+        
+        var slow = nums[0]
+        var fast = nums[0]
+        
+        repeat {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        } while slow != fast
+        
+        
+        slow = nums[0]
+        
+        while slow != fast {
+            slow = nums[slow]
+            fast = nums[fast]
+        }
+        
+        return slow
+    }
+}
