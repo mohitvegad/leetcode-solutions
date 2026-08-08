9aac7f0ee5cc9bd08f58e2da9a61d62b264923c9
mohitvegad <kfdvhtq424@privaterelay.appleid.com>
2026-08-08 01:08:46 +0100

    Time: 43 ms (98.37%), Space: 19.9 MB (82.57%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,20 @@
+from threading import Event
+
+class Foo:
+
+    def __init__(self):
+        self.first_done = Event()
+        self.second_done = Event()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        printFirst()
+        self.first_done.set()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.first_done.wait()
+        printSecond()
+        self.second_done.set()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.second_done.wait()
+        printThird()
