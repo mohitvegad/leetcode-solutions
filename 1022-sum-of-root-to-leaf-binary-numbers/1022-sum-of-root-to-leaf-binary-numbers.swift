1d9f57ba88991ad9aad6b7f3916242037017f52f
mohitvegad <kfdvhtq424@privaterelay.appleid.com>
2026-09-03 23:24:02 +0100

    Time: 0 ms (100%), Space: 19.3 MB (100%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func sumRootToLeaf(_ root: TreeNode?) -> Int {
+        return dfs(root, 0)
+    }
+
+    private func dfs(_ node: TreeNode?, _ curr: Int) -> Int {
+        guard let node = node else {
+            return 0
+        }
+
+        let value = curr * 2 + node.val
+
+        if node.left == nil && node.right == nil {
+            return value
+        }
+
+        let left = dfs(node.left, value)
+        let right = dfs(node.right, value)
+
+        return left + right
+    }
+}
