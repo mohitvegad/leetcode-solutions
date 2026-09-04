f29cbaaee18f746cec1f8cc972a4fb48e7a95e5c
mohitvegad <kfdvhtq424@privaterelay.appleid.com>
2026-09-04 23:54:55 +0100

    Time: 0 ms (100%), Space: 19.5 MB (90%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
+        var left = 0
+        var right = letters.count - 1
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            
+            if letters[mid] <= target {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        
+        return letters[left % letters.count]
+    }
+}
