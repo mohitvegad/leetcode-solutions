89b749d666c7eaaaf1e75f404b03e88644ce9b4e
mohitvegad <kfdvhtq424@privaterelay.appleid.com>
2026-09-06 22:17:07 +0100

    Time: 0 ms (100%), Space: 22.2 MB (7.69%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.swift
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func findBottomLeftValue(_ root: TreeNode?) -> Int {
+        
+        var answer = 0
+        var maxDepth = -1
+        
+        func dfs(_ node: TreeNode?, _ depth: Int) {
+            
+            guard let node = node else {
+                return
+            }
+            
+            if depth > maxDepth {
+                maxDepth = depth
+                answer = node.val
+            }
+            
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        }
+        
+        dfs(root, 0)
+        
+        return answer
+    }
+}
